3a4987aa5e590c30dc26c6ae574f166584203ba1
Divij Bindlish <dvjbndlsh93@gmail.com>
2015-05-21 00:25:29 +0530

    Remove synchronous method to generate hash

--- index.js
+++ index.js
@@ -57,25 +57,6 @@ var generateHash = function (filename, callback) {
 	});
 };
 
-var generateHashSync = function (filename) {
-	if (!isVideo(filename)) {
-		throw new Error('Invalid video file');
-	}
-
-	var fd = fs.openSync(filename, 'r');
-	var stats = fs.fstatSync(fd);
-	var readsize = 1 << 16;
-	var buffer = new Buffer(readsize << 1);
-
-	var position = stats['size'] - readsize;
-
-	fs.readSync(fd, buffer, 0, readsize, 0);
-	fs.readSync(fd, buffer, readsize, readsize, position);
-
-	return md5(buffer);
-};
-
 module.exports = {
-	generateHash: generateHash,
-	generateHashSync: generateHashSync
+	generateHash: generateHash
 };
