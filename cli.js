d366dedb31773c11ec21ff5aeb87d92fb0e4c40d
Divij Bindlish <dvjbndlsh93@gmail.com>
2015-05-24 03:06:51 +0530

    Add error handling in CLI. Print error message to STDOUT

--- cli.js
+++ cli.js
@@ -5,6 +5,37 @@ var program = require('commander');
 var subfil = require('./');
 var pkg = require('./package');
 var videoExtensions = require('video-extensions');
+var allLanguages = require('./languages')
+
+var NOOP = function () {};
+
+var handleFile = function (file, callback) {
+	callback = arguments[arguments.length - 1];
+	if (typeof callback !== 'function') {
+		callback = NOOP;
+	}
+
+	console.log('Downloading subtitles for ' + path.basename(file));
+	subfil.download(file, language, destination, function (err, dest) {
+		if (err) {
+			switch (err.message) {
+				case 'Invalid video file':
+					console.log('Please specify a valid video file');
+					break;
+
+				case 'No subtitles for hash in specified language':
+					console.log('No subtitles available')
+					break;
+
+				default:
+					callback(err);
+					return;
+			}
+		} else {
+			console.log('Subtitles downloaded successfully');
+		}
+	});
+}
 
 program
 	.version(pkg.version)
@@ -17,6 +48,15 @@ program
 if (program.args.length === 0) {
 	console.error('You need to specify at least one path');
 	process.exit(1);
+}
+
+if (typeof program.language === 'string') {
+	if (allLanguages.indexOf(program.language.toLowerCase()) === -1) {
+		console.log('Invalid language provided');
+		console.log('Please choose a language from the following:');
+		console.log(allLanguages.join(', ').join('\n'));
+		process.exit(1);
+	}
 }
 
 var i, files = [];
@@ -32,11 +72,8 @@ var destination = program.destination;
 
 var file = files[0];
 
-console.log('Downloading subtitles for ' + file);
-subfil.download(file, language, destination, function (err, dest) {
+handleFile(file, language, destination, function (err) {
 	if (err) {
 		throw err;
-	} else {
-		console.log('Subtitles successfully downloaded in ' + dest);
 	}
 });
