0177c0c86757ea2de2928010c1fe50045cb7964a
Divij Bindlish <dvjbndlsh93@gmail.com>
2015-05-24 16:37:58 +0530

    Shift to spaces

--- cli.js
+++ cli.js
@@ -13,141 +13,141 @@ var allLanguages = require('./languages')
 var NOOP = function () {};
 
 var handleFile = function (file, language, callback) {
-	callback = arguments[arguments.length - 1];
-	if (typeof callback !== 'function') {
-		callback = NOOP;
-	}
+  callback = arguments[arguments.length - 1];
+  if (typeof callback !== 'function') {
+    callback = NOOP;
+  }
 
-	console.log('Downloading subtitles for ' + path.basename(file));
-	subfil.download(file, language, function (err, dest) {
-		if (err) {
-			switch (err.message) {
-				case 'Invalid video file':
-					console.log('Please specify a valid video file');
-					callback(undefined, false);
-					break;
+  console.log('Downloading subtitles for ' + path.basename(file));
+  subfil.download(file, language, function (err, dest) {
+    if (err) {
+      switch (err.message) {
+        case 'Invalid video file':
+          console.log('Please specify a valid video file');
+          callback(undefined, false);
+          break;
 
-				case 'No subtitles for hash':
-					console.log('No subtitles available in specified language')
-					callback(undefined, false);
-					break;
+        case 'No subtitles for hash':
+          console.log('No subtitles available in specified language')
+          callback(undefined, false);
+          break;
 
-				default:
-					callback(err, undefined);
-			}
-		} else {
-			console.log('Subtitles downloaded successfully');
-			callback(undefined, true);
-		}
-	});
+        default:
+          callback(err, undefined);
+      }
+    } else {
+      console.log('Subtitles downloaded successfully');
+      callback(undefined, true);
+    }
+  });
 };
 
 var handleMultipleFiles = function (files, language, callback) {
-	var tasks = [];
-	files.forEach(function (file) {
-		tasks.push(function (callback) {
-			handleFile(file, language, callback);
-		});
-	});
+  var tasks = [];
+  files.forEach(function (file) {
+    tasks.push(function (callback) {
+      handleFile(file, language, callback);
+    });
+  });
 
-	async.series(tasks, function (err, results) {
-		if (err) {
-			callback(err, undefined);
-			return;
-		}
+  async.series(tasks, function (err, results) {
+    if (err) {
+      callback(err, undefined);
+      return;
+    }
 
-		callback(undefined, results);
-	});
+    callback(undefined, results);
+  });
 };
 
 var handleDirectory = function (directory, callback) {
-	fs.lstat(directory, function (err, stats) {
-		if (err) {
-			callback(err, undefined);
-			return;
-		}
+  fs.lstat(directory, function (err, stats) {
+    if (err) {
+      callback(err, undefined);
+      return;
+    }
 
-		if (stats.isDirectory()) {
-			var extensionFilter = '{' + videoExtensions.join(',') + '}';
-			glob(directory + '/**/*.' + extensionFilter, function (err, files) {
-				if (err) {
-					callback(err, undefined);
-					return;
-				}
+    if (stats.isDirectory()) {
+      var extensionFilter = '{' + videoExtensions.join(',') + '}';
+      glob(directory + '/**/*.' + extensionFilter, function (err, files) {
+        if (err) {
+          callback(err, undefined);
+          return;
+        }
 
-				callback(undefined, files);
-			});
-		} else {
-			callback(undefined, directory);
-		}
-	});
+        callback(undefined, files);
+      });
+    } else {
+      callback(undefined, directory);
+    }
+  });
 };
 
 var handleMultipleDirectories = function (directories, callback) {
-	var tasks = [];
-	directories.forEach(function (directory) {
-		tasks.push(function (callback) {
-			handleDirectory(directory, callback);
-		});
-	});
+  var tasks = [];
+  directories.forEach(function (directory) {
+    tasks.push(function (callback) {
+      handleDirectory(directory, callback);
+    });
+  });
 
-	async.series(tasks, function (err, files) {
-		if (err) {
-			callback(err, undefined);
-			return;
-		}
+  async.series(tasks, function (err, files) {
+    if (err) {
+      callback(err, undefined);
+      return;
+    }
 
-		callback(undefined, files);
-	});
+    callback(undefined, files);
+  });
 };
 
 program
-	.version(pkg.version)
-	.usage('subfil <path> [<path> ...]')
-	.option('-r, --recursive', 'download subtitles for files recursively')
-	.option('-l, --language <language>', 'download subtitles in given language')
-	.parse(process.argv);
+  .version(pkg.version)
+  .usage('subfil <path> [<path> ...]')
+  .option('-r, --recursive', 'download subtitles for files recursively')
+  .option('-l, --language <language>', 'download subtitles in given language')
+  .parse(process.argv);
 
 if (program.args.length === 0) {
-	console.error('You need to specify at least one path');
-	process.exit(1);
+  console.error('You need to specify at least one path');
+  process.exit(1);
 }
 
 if (typeof program.language === 'string') {
-	if (allLanguages.indexOf(program.language.toLowerCase()) === -1) {
-		console.log('Invalid language provided');
-		console.log('Please choose a language from the following:');
-		console.log(allLanguages.join(', ').join('\n'));
-		process.exit(1);
-	}
+  if (allLanguages.indexOf(program.language.toLowerCase()) === -1) {
+    console.log('Invalid language provided');
+    console.log('Please choose a language from the following:');
+    console.log(allLanguages.join(', ').join('\n'));
+    process.exit(1);
+  }
 }
 
 var language = program.language;
 var files = [];
 
 var finalCallback = function (err, results) {
-	if (err) {
-		throw err;
-	}
+  if (err) {
+    throw err;
+  }
 
-	var total = 0;
-	results.forEach(function (value) {
-		total += value;
-	});
-	console.log('Downloaded a total of ' + total + ' subtitle(s)');
+  var total = 0;
+  results.forEach(function (value) {
+    total += value;
+  });
+  console.log('Downloaded a total of ' + total + ' subtitle(s)');
 };
 
 if (program.recursive) {
-	handleMultipleDirectories(program.args, function (err, results) {
-		if (err) {
-			finalCallback(err, undefined);
-			return;
-		}
+  handleMultipleDirectories(program.args, function (err, results) {
+    if (err) {
+      finalCallback(err, undefined);
+      return;
+    }
 
-		var files = [].concat.apply([], results);
-		handleMultipleFiles(files, language, finalCallback);
-	});
+    var files = [].concat.apply([], results);
+    handleMultipleFiles(files, language, finalCallback);
+  });
 } else {
-	files = program.args;
-	handleMultipleFiles(files, language, finalCallback);
+  files = program.args;
+  handleMultipleFiles(files, language, finalCallback);
 }
--- index.js
+++ index.js
@@ -12,228 +12,228 @@ var request = require('request');
 var NOOP = function () {};
 
 var USER_AGENT_STRING = 'SubDB/1.0 (subfil/0.1; '
-	+ 'https://github.com/divijbindlish/subfil)';
+  + 'https://github.com/divijbindlish/subfil)';
 
 var generateHash = function (filename, callback) {
-	callback = arguments[arguments.length - 1];
-	if (typeof callback !== 'function') {
-		callback = NOOP;
-	}
+  callback = arguments[arguments.length - 1];
+  if (typeof callback !== 'function') {
+    callback = NOOP;
+  }
 
-	var ext = path.extname(filename).slice(1);
-	if (videoExtensions.indexOf(ext) === -1) {
-		var err = new Error('Invalid video file');
-		callback(err, undefined);
-		return;
-	}
+  var ext = path.extname(filename).slice(1);
+  if (videoExtensions.indexOf(ext) === -1) {
+    var err = new Error('Invalid video file');
+    callback(err, undefined);
+    return;
+  }
 
-	fs.open(filename, 'r', function (err, fd) {
-		if (err) {
-			callback(err, undefined);
-			return;
-		}
+  fs.open(filename, 'r', function (err, fd) {
+    if (err) {
+      callback(err, undefined);
+      return;
+    }
 
-		fs.fstat(fd, function (err, stats) {
-			if (err) {
-				callback(err, undefined);
-				return;
-			}
+    fs.fstat(fd, function (err, stats) {
+      if (err) {
+        callback(err, undefined);
+        return;
+      }
 
-			var readsize = 1 << 16;
-			var buffer = new Buffer(readsize << 1);
+      var readsize = 1 << 16;
+      var buffer = new Buffer(readsize << 1);
 
-			fs.read(fd, buffer, 0, readsize, 0, function (err) {
-				if (err) {
-					callback(err, undefined);
-					return;
-				}
+      fs.read(fd, buffer, 0, readsize, 0, function (err) {
+        if (err) {
+          callback(err, undefined);
+          return;
+        }
 
-				var position = stats['size'] - readsize;
-				fs.read(fd, buffer, readsize, readsize, position, function(err) {
-					if (err) {
-						callback(err, undefined);
-						return;
-					}
+        var position = stats['size'] - readsize;
+        fs.read(fd, buffer, readsize, readsize, position, function(err) {
+          if (err) {
+            callback(err, undefined);
+            return;
+          }
 
-					var hash = md5(buffer);
-					callback(undefined, hash);
-				});
-			});
-		});
-	});
+          var hash = md5(buffer);
+          callback(undefined, hash);
+        });
+      });
+    });
+  });
 };
 
 var getLanguagesForHash = function (hash, callback) {
-	callback = arguments[arguments.length - 1];
-	if (typeof callback !== 'function') {
-		callback = NOOP;
-	}
+  callback = arguments[arguments.length - 1];
+  if (typeof callback !== 'function') {
+    callback = NOOP;
+  }
 
-	if (!hash.match(/^[a-f0-9]{32}$/)) {
-		var err = new Error('Invalid MD5 hash');
-		callback(err, undefined);
-		return;
-	}
+  if (!hash.match(/^[a-f0-9]{32}$/)) {
+    var err = new Error('Invalid MD5 hash');
+    callback(err, undefined);
+    return;
+  }
 
-	request({
-		url: 'http://api.thesubdb.com',
-		qs: {
-			action: 'search',
-			hash: hash,
-		},
-		method: 'GET',
-		headers: {
-			'User-Agent': USER_AGENT_STRING,
-		}
-	}, function (err, response, body) {
-		if (err) {
-			callback(err, undefined);
-			return;
-		}
+  request({
+    url: 'http://api.thesubdb.com',
+    qs: {
+      action: 'search',
+      hash: hash,
+    },
+    method: 'GET',
+    headers: {
+      'User-Agent': USER_AGENT_STRING,
+    }
+  }, function (err, response, body) {
+    if (err) {
+      callback(err, undefined);
+      return;
+    }
 
-		if (response.statusCode === 404) {
-			err = new Error('No subtitles for hash');
-			callback(err, undefined);
-			return;
-		}
+    if (response.statusCode === 404) {
+      err = new Error('No subtitles for hash');
+      callback(err, undefined);
+      return;
+    }
 
-		if (response.statusCode !== 200) {
-			err = new Error('Invalid request');
-			callback(err, undefined);
-			return;
-		}
+    if (response.statusCode !== 200) {
+      err = new Error('Invalid request');
+      callback(err, undefined);
+      return;
+    }
 
-		var languages = response.body.split(',');
-		callback(undefined, languages);
-	});
+    var languages = response.body.split(',');
+    callback(undefined, languages);
+  });
 };
 
 var downloadForHash = function (hash, language, destination, callback) {
-	request({
-		url: 'http://api.thesubdb.com',
-		qs: {
-			action: 'download',
-			language: language,
-			hash: hash,
-		},
-		method: 'GET',
-		headers: {
-			'User-Agent': USER_AGENT_STRING,
-		}
-	}, function(err, response, body) {
-		if (err) {
-			callback(err, undefined);
-			return;
-		}
+  request({
+    url: 'http://api.thesubdb.com',
+    qs: {
+      action: 'download',
+      language: language,
+      hash: hash,
+    },
+    method: 'GET',
+    headers: {
+      'User-Agent': USER_AGENT_STRING,
+    }
+  }, function(err, response, body) {
+    if (err) {
+      callback(err, undefined);
+      return;
+    }
 
-		if (response.statusCode === 404) {
-			err = new Error('No subtitles for hash');
-			callback(err, undefined);
-			return;
-		}
+    if (response.statusCode === 404) {
+      err = new Error('No subtitles for hash');
+      callback(err, undefined);
+      return;
+    }
 
-		if (response.statusCode !== 200) {
-			err = new Error('Invalid request');
-			callback(err, undefined);
-			return;
-		}
+    if (response.statusCode !== 200) {
+      err = new Error('Invalid request');
+      callback(err, undefined);
+      return;
+    }
 
-		fs.open(destination, 'w', function (err, fd) {
-			if (err) {
-				callback(err, undefined);
-				return;
-			}
+    fs.open(destination, 'w', function (err, fd) {
+      if (err) {
+        callback(err, undefined);
+        return;
+      }
 
-			fs.write(fd, body, function (err, bytesWritten, string) {
-				if (err) {
-					callback(err, undefined);
-					return;
-				}
+      fs.write(fd, body, function (err, bytesWritten, string) {
+        if (err) {
+          callback(err, undefined);
+          return;
+        }
 
-				fs.close(fd, function (err) {
-					if (err) {
-						callback(err, undefined);
-						return;
-					}
+        fs.close(fd, function (err) {
+          if (err) {
+            callback(err, undefined);
+            return;
+          }
 
-					callback(undefined, destination);
-				});
-			});
-		});
-	});
+          callback(undefined, destination);
+        });
+      });
+    });
+  });
 };
 
 var getLanguages = function (name, callback) {
-	callback = arguments[arguments.length - 1];
-	if (typeof callback !== 'function') {
-		callback = NOOP;
-	}
+  callback = arguments[arguments.length - 1];
+  if (typeof callback !== 'function') {
+    callback = NOOP;
+  }
 
-	if (name.match(/^[a-f0-9]{32}$/)) {
-		// Process for hash
-		var hash = name;
-		getLanguagesForHash(hash, callback);
-	} else {
-		// Process for file
-		var filename = name;
-		generateHash(filename, function (err, hash) {
-			if (err) {
-				callback(err, undefined);
-				return;
-			}
+  if (name.match(/^[a-f0-9]{32}$/)) {
+    // Process for hash
+    var hash = name;
+    getLanguagesForHash(hash, callback);
+  } else {
+    // Process for file
+    var filename = name;
+    generateHash(filename, function (err, hash) {
+      if (err) {
+        callback(err, undefined);
+        return;
+      }
 
-			getLanguagesForHash(hash, callback);
-		});
-	}
+      getLanguagesForHash(hash, callback);
+    });
+  }
 };
 
 var download = function (name, language, destination, callback) {
-	var separator = /^win/.test(process.platform) ? '\\' : '/';
+  var separator = /^win/.test(process.platform) ? '\\' : '/';
 
-	callback = arguments[arguments.length - 1];
-	if (typeof callback !== 'function') {
-		callback = NOOP;
-	}
+  callback = arguments[arguments.length - 1];
+  if (typeof callback !== 'function') {
+    callback = NOOP;
+  }
 
-	if (typeof language !== 'string') {
-		language = 'en';
-	} else {
-		if (allLanguages.indexOf(language) === -1) {
-			var err = new Error('Invalid language');
-			callback(err, undefined);
-			return;
-		}
-	}
+  if (typeof language !== 'string') {
+    language = 'en';
+  } else {
+    if (allLanguages.indexOf(language) === -1) {
+      var err = new Error('Invalid language');
+      callback(err, undefined);
+      return;
+    }
+  }
 
-	if (name.match(/^[a-f0-9]{32}$/)) {
-		// Process for hash
-		var hash = name;
-		if (typeof destination !== 'string') {
-			destination = os.tmpdir() + separator + hash + '.srt';
-		}
+  if (name.match(/^[a-f0-9]{32}$/)) {
+    // Process for hash
+    var hash = name;
+    if (typeof destination !== 'string') {
+      destination = os.tmpdir() + separator + hash + '.srt';
+    }
 
-		downloadForHash(hash, language, destination, callback);
-	} else {
-		// Process for file
-		var filename = name;
-		generateHash(filename, function (err, hash) {
-			if (err) {
-				callback(err, undefined);
-				return;
-			}
+    downloadForHash(hash, language, destination, callback);
+  } else {
+    // Process for file
+    var filename = name;
+    generateHash(filename, function (err, hash) {
+      if (err) {
+        callback(err, undefined);
+        return;
+      }
 
-			if (typeof destination !== 'string') {
-				destination = path.dirname(filename) + separator
-					+ path.basename(filename, path.extname(filename))
-					+ '-' + language + '.srt';
-			}
+      if (typeof destination !== 'string') {
+        destination = path.dirname(filename) + separator
+          + path.basename(filename, path.extname(filename))
+          + '-' + language + '.srt';
+      }
 
-			downloadForHash(hash, language, destination, callback);
-		});
-	}
+      downloadForHash(hash, language, destination, callback);
+    });
+  }
 };
 
 module.exports = {
-	getLanguages: getLanguages,
-	download: download
+  getLanguages: getLanguages,
+  download: download
 };
